46f223b37d052f62c6c91527c030b9065aad7988
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-14 18:39:20 +0200

    update index

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -92,7 +92,6 @@
         </nav>
       </header>
 
-
       <!-- Left side column. contains the logo and sidebar -->
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
