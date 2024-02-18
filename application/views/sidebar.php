a4a3512174fd7befacccea2b916c7bbfe868be34
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-18 09:34:40 +0200

    make sidebar.php

---
+++ application/views/sidebar.php
@@ -0,0 +1,167 @@
+<div class="page-sidebar ">
+	<!-- MAIN MENU - START -->
+	<div class="page-sidebar-wrapper" id="main-menu-wrapper"> 
+
+		<!-- USER INFO - START -->
+		<div class="profile-info row">
+
+			<div class="profile-image col-lg-4 col-md-4 col-4">
+				<a href="ui-profile.html">
+					<img src="data/profile/profile-crm.jpg" class="img-fluid rounded-circle">
+				</a>
+			</div>
+
+			<div class="profile-details col-lg-8 col-md-8 col-8">
+
+				<h3>
+					<a href="ui-profile.html">John Wilson</a>
+
+					<!-- Available statuses: online, idle, busy, away and offline -->
+					<span class="profile-status online"></span>
+				</h3>
+
+				<p class="profile-title">Administrator</p>
+
+			</div>
+
+		</div>
+		<!-- USER INFO - END -->
+
+
+
+		<ul class='wraplist'>	
+
+
+			<li class=""> 
+				<a href="index-crm.html">
+					<i class="fa fa-dashboard"></i>
+					<span class="title">Dashboard</span>
+				</a>
+			</li>
+			<li class=""> 
+				<a href="javascript:;">
+					<i class="fa fa-users"></i>
+					<span class="title">Customers</span>
+					<span class="arrow "></span>
+				</a>
+				<ul class="sub-menu" >
+					<li>
+						<a class="" href="crm-customers.html" >All Customers</a>
+					</li>
+					<li>
+						<a class="" href="crm-customer-add.html" >Add Customer</a>
+					</li>
+					<li>
+						<a class="" href="crm-customer-edit.html" >Edit Customer</a>
+					</li>
+				</ul>
+			</li>
+			<li class=""> 
+				<a href="javascript:;">
+					<i class="fa fa-users"></i>
+					<span class="title">Leads</span>
+					<span class="arrow "></span>
+				</a>
+				<ul class="sub-menu" >
+					<li>
+						<a class="" href="crm-leads.html" >All Leads</a>
+					</li>
+					<li>
+						<a class="" href="crm-lead-add.html" >Add Lead</a>
+					</li>
+					<li>
+						<a class="" href="crm-lead-edit.html" >Edit Lead</a>
+					</li>
+				</ul>
+			</li>
+			<li class=""> 
+				<a href="javascript:;">
+					<i class="fa fa-dollar "></i>
+					<span class="title">Invoices</span>
+					<span class="arrow "></span>
+				</a>
+				<ul class="sub-menu" >
+					<li>
+						<a class="" href="crm-invoices.html" >All Invoices</a>
+					</li>
+					<li>
+						<a class="" href="crm-invoice-add.html" >Add Invoice</a>
+					</li>
+					<li>
+						<a class="" href="crm-invoice-edit.html" >Edit Invoice</a>
+					</li>
+					<li>
+						<a class="" href="crm-invoice-view.html" >View Invoice</a>
+					</li>
+				</ul>
+			</li>
+			<li class=""> 
+				<a href="javascript:;">
+					<i class="fa fa-phone"></i>
+					<span class="title">Contacts</span>
+					<span class="arrow "></span>
+				</a>
+				<ul class="sub-menu" >
+					<li>
+						<a class="" href="crm-contacts.html" >All Contacts</a>
+					</li>
+					<li>
+						<a class="" href="crm-contact-add.html" >Add Contact</a>
+					</li>
+					<li>
+						<a class="" href="crm-contact-edit.html" >Edit Contact</a>
+					</li>
+				</ul>
+			</li>
+			<li class=""> 
+				<a href="crm-calendar.html">
+					<i class="fa fa-calendar"></i>
+					<span class="title">Calendar</span>
+				</a>
+			</li>
+			<li class="open"> 
+				<a href="javascript:;">
+					<i class="fa fa-user"></i>
+					<span class="title">Users</span>
+					<span class="arrow open"></span>
+				</a>
+				<ul class="sub-menu" style='display:block;'>
+					<li>
+						<a class="active" href="crm-users.html" >All Users</a>
+					</li>
+					<li>
+						<a class="" href="crm-user-add.html" >Add User</a>
+					</li>
+					<li>
+						<a class="" href="crm-user-edit.html" >Edit User</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+
+	</div>
+	<!-- MAIN MENU - END -->
+
+	<div class="project-info">
+
+		<div class="block1">
+			<div class="data">
+				<span class='title'>Target</span>
+				<span class='total'>80%</span>
+			</div>
+			<div class="graph">
+				<span class="sidebar_orders">...</span>
+			</div>
+		</div>
+
+		<div class="block2">
+			<div class="data">
+				<span class='title'>Customers</span>
+				<span class='total'>3146</span>
+			</div>
+			<div class="graph">
+				<span class="sidebar_visitors">...</span>
+			</div>
+		</div>
+	</div>
+</div>
