1d976a91efae2ee649bba86e5721bee8d143d4e0
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-18 09:47:54 +0200

    update footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,5 @@
+	
+	</div>
 	<!-- jQuery 2.1.3 -->
 	<script src="<?php echo base_url('assets/plugins/jQuery/jQuery-2.1.3.min.js');?>"></script>
 	<!-- jQuery UI 1.11.2 -->
@@ -37,7 +39,43 @@
     <script src="<?php echo base_url('assets/plugins/datatables/jquery.dataTables.js');?>" type="text/javascript"></script>
     <script src="<?php echo base_url('assets/plugins/datatables/dataTables.bootstrap.js');?>" type="text/javascript"></script>
     <script src="<?php echo base_url('assets/plugins/ckeditor/ckeditor.js');?>"></script>
-		
+		<!-- CORE JS FRAMEWORK - START --> 
+		<script src="<?php echo base_url('assets/js/jquery-3.4.1.min.js');?>" type="text/javascript"></script> 
+		<script src="<?php echo base_url('assets/js/popper.min.js');?>" type="text/javascript"></script> 
+		<!-- <script src="<?php echo base_url('assets/js/jquery.easing.min.js');?>" type="text/javascript"></script>  -->
+		<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.min.js');?>" type="text/javascript"></script> 
+		<script src="<?php echo base_url('assets/plugins/pace/pace.min.js');?>" type="text/javascript"></script>  
+
+		<script src="<?php echo base_url('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js');?>" type="text/javascript"></script> 
+		<script src="<?php echo base_url('assets/plugins/viewport/viewportchecker.js');?>" type="text/javascript"></script>  
+		<!-- CORE JS FRAMEWORK - END --> 
+
+
+		<!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START --> 
+		<script src="<?php echo base_url('assets/plugins/datatables/js/dataTables.min.js');?>" type="text/javascript"></script> <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js">
+		</script>
+		<script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js">
+		</script> <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END --> 
+
+
+		<!-- CORE TEMPLATE JS - START --> 
+		<script src="<?php echo base_url('assets/js/scripts.js');?>" type="text/javascript"></script> 
+		<!-- END CORE TEMPLATE JS - END --> 
+
+		<!-- Sidebar Graph - START --> 
+		<script src="<?php echo base_url('assets/plugins/sparkline-chart/jquery.sparkline.min.js');?>" type="text/javascript"></script>
+		<script src="<?php echo base_url('assets/js/chart-sparkline.js');?>" type="text/javascript"></script>
+		<!-- Sidebar Graph - END --> 
     <script type="text/javascript">
 			$(function () {
 				$('#SearchTable').dataTable({
