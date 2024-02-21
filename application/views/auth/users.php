9ded4174ce59d8eda17e87c9f1b8a61ade2ae537
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-21 18:23:23 +0200

    update users.php

--- application/views/auth/users.php
+++ application/views/auth/users.php
@@ -17,7 +17,6 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
 
