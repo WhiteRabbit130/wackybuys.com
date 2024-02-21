a6cd02390a5fafe6571cfde385e9f9a8e44a970f
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-21 18:20:28 +0200

    update edit_user

--- application/views/auth/edit_user.php
+++ application/views/auth/edit_user.php
@@ -17,10 +17,8 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-
               <!-- Messages: style can be found in dropdown.less-->
               <li class="dropdown messages-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
