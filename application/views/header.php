638ffd4a699e65a0b4feac5001c8368bedc3174b
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-22 16:29:37 +0200

    update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -19,7 +19,7 @@
 		<!-- CORE CSS FRAMEWORK - START -->
 		<link href="<?php echo base_url('assets/plugins/pace/pace-theme-flash.css');?>" rel="stylesheet" type="text/css" media="screen"/>
 		<link href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo base_url('assets/fonts/font-awesome/css/font-awesome.css');?>" rel="stylesheet" type="text/css"/>
+    	<link href="<?php echo base_url('assets/fonts/font-awesome/css/font-awesome.css');?>" rel="stylesheet" type="text/css"/>
 		<link href="<?php echo base_url('assets/css/animate.min.css');?>" rel="stylesheet" type="text/css"/>
 		<link href="<?php echo base_url('assets/plugins/perfect-scrollbar/perfect-scrollbar.css');?>" rel="stylesheet" type="text/css"/>
 		<!-- CORE CSS FRAMEWORK - END -->
