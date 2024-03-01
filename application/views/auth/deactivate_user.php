7f2dfb86aacf5b8ac018db8fd58a25ee566f6ae0
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-01 20:06:19 +0200

    update deactivate_user

--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -3,25 +3,25 @@
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 			<h4 class="modal-title" id="myModalLabel"><?php echo lang('deactivate_heading');?></h4>
 	    </div>
-	                <div class="modal-body">
+		<div class="modal-body">
 
-	                  <div class="form-group">
-	                  <?php echo sprintf(lang('deactivate_subheading'), '<strong>'.$user->username.'?'.'</strong>');?> 
-	                  </div>
+			<div class="form-group">
+			<?php echo sprintf(lang('deactivate_subheading'), '<strong>'.$user->username.'?'.'</strong>');?> 
+			</div>
 
-						<div class="btn-group" data-toggle="buttons">
-				                <label class="btn btn-default active">
-				                    <input type="radio" name="confirm" value="yes" checked="checked" /> yes
-				                </label> 
-				                <label class="btn btn-default">
-				                    <input type="radio" name="confirm" value="no" /> no
-				                </label> 
-	            		</div>
+			<div class="btn-group" data-toggle="buttons">
+					<label class="btn btn-default active">
+						<input type="radio" name="confirm" value="yes" checked="checked" /> yes
+					</label> 
+					<label class="btn btn-default">
+						<input type="radio" name="confirm" value="no" /> no
+					</label> 
+			</div>
 
-	            		<?php echo form_hidden($csrf); ?>
-					  	<?php echo form_hidden(array('id'=>$user->id)); ?>   
+			<?php echo form_hidden($csrf); ?>
+			<?php echo form_hidden(array('id'=>$user->id)); ?>   
 
-	                </div>
+		</div>
 
 	    <div class="modal-footer">
 	    	<?php $data = array('type'  =>'submit',
