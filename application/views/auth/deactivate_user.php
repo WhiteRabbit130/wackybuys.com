3b98ef4b4898bcdc3dac0e2b1cdb7f5a3fce1ad4
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-01 20:05:48 +0200

    update deactivate

--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -1,7 +1,7 @@
 <?php echo form_open("auth/deactivate/".$user->id);?>
 	    <div class="modal-header">
-	                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	                <h4 class="modal-title" id="myModalLabel"><?php echo lang('deactivate_heading');?></h4>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<h4 class="modal-title" id="myModalLabel"><?php echo lang('deactivate_heading');?></h4>
 	    </div>
 	                <div class="modal-body">
 
